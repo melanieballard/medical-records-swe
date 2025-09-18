af28c1a28d268d21c01abd83c2b2702082db6336
Melanie Ballard <mballard7@storm.cis.fordham.edu>
2025-09-18 16:56:31 -0400

    blue background and medical record title

--- medical-records/src/App.jsx
+++ medical-records/src/App.jsx
@@ -5,9 +5,11 @@ import './App.css'
 function App() {
 
   return (
-    <Container fluid>
-      <Row>
-        <Col>Medical Records</Col>
+    <Container fluid className="d-flex vh-100">
+      <Row className="m-auto text-center">
+        <Col>
+          <h1> Medical Records </h1>
+        </Col>
       </Row>
     </Container>
   )
