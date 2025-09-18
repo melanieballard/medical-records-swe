9c08ba00aac0fddf795cbc844a660f4813c7392d
Melanie Ballard <mballard7@storm.cis.fordham.edu>
2025-09-18 16:11:56 -0400

    add react bootstrap and medical logo

--- medical-records/vite.config.js
+++ medical-records/vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "./", // ensures relative paths work in public_html
+  optimizeDeps: {
+    include: ['bootstrap']
+  }
 })
